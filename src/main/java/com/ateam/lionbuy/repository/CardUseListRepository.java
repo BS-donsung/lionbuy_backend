0b9d5d1b84dd7026561430aac7ee911954ce3cec
minah-moon <118171802+minah-moon@users.noreply.github.com>
2023-04-10 17:00:28 +0900

    minah2

--- src/main/java/com/ateam/lionbuy/repository/CardUseListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ateam.lionbuy.repository;
-
-import com.ateam.lionbuy.entity.Card_use_list;
-import com.ateam.lionbuy.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardUseListRepository extends JpaRepository<Card_use_list, Long> {
-
-}
