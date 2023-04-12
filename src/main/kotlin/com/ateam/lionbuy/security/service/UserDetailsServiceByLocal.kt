13fbefb859441266c95642481aa96a4d8fb3e15d
james <yahvz01@gmail>
2023-04-12 14:47:33 +0900

    login mocking

--- src/main/kotlin/com/ateam/lionbuy/security/service/UserDetailsServiceByLocal.kt
+++ src/main/kotlin/com/ateam/lionbuy/security/service/UserDetailsServiceByLocal.kt
@@ -1,5 +1,6 @@
 package com.ateam.lionbuy.security.service
 
+import com.ateam.lionbuy.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.security.core.context.SecurityContextHolder
@@ -13,62 +14,16 @@ import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.stereotype.Component
 
 
-//@Component
-//class UserDetailsServiceByLocal : UserDetailsService {
-//    val testRepository = listOf(User("james", "1234", emptyList()))
-//
-//    override fun loadUserByUsername(username: String): UserDetails {
-//        val queryResult = testRepository.findLast { it.username == username }
-//        return queryResult?.let { queryResult } ?: throw UsernameNotFoundException("User Not Found")
-//    }
-//
-//    @Bean
-//    fun passwordEncoder() : PasswordEncoder = NoOpPasswordEncoder.getInstance()
-//}
-
-
 @Component
 class UserDetailsServiceByLocal
 @Autowired constructor(
-    val passwordEncoder : PasswordEncoder
-): UserDetailsManager {
+    val authRepository : UserRepository
+): UserDetailsService {
 
-    val testRepository = mutableListOf<UserDetails>(User("james", "1234", emptyList()))
-
+    // Mocking
+    val testRepository = mutableListOf<UserDetails>(User("yahvz01@gmail.com", "1234", emptyList()))
     override fun loadUserByUsername(username: String): UserDetails {
         val queryResult = testRepository.findLast { it.username == username }
         return queryResult?.let { queryResult } ?: throw UsernameNotFoundException("User Not Found")
     }
-
-    override fun createUser(user: UserDetails) {
-        testRepository.add(user)
-    }
-
-    override fun updateUser(user: UserDetails) {
-        val idx = testRepository.indexOfLast { it.username == user.username }
-        if(idx != -1)
-            testRepository.set(idx, user)
-    }
-
-    override fun deleteUser(username: String) {
-        val idx = testRepository.indexOfLast { it.username == username }
-        if(idx != -1)
-            testRepository.removeAt(idx)
-    }
-
-    override fun changePassword(oldPassword: String, newPassword: String) {
-        val targetUserName = SecurityContextHolder.getContext().authentication.name
-        testRepository.findLast { it.username == targetUserName }?.let {
-            if(passwordEncoder.matches(it.password, oldPassword)) {
-                val idx = testRepository.indexOfLast { it.username == targetUserName }
-                testRepository.set(idx, User(it.username, passwordEncoder.encode(oldPassword), emptyList()))
-            }
-        }
-    }
-
-    override fun userExists(username: String): Boolean {
-        val idx = testRepository.indexOfLast { it.username == username }
-        return (idx != -1)
-    }
-
 }
