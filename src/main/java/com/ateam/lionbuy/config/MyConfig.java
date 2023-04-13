e908e408cfd523eb1ed9818803c55c159360130f
KimBoKwon <dkdn00500@gmail.com>
2023-04-13 09:59:29 +0900

    kotlin security 삭제

--- src/main/java/com/ateam/lionbuy/config/MyConfig.java
+++ src/main/java/com/ateam/lionbuy/config/MyConfig.java
@@ -1,7 +1,7 @@
 package com.ateam.lionbuy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
