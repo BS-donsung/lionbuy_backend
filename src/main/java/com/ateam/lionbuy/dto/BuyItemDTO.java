bd62f97bbd4dd3670f627a124541456a55753a90
minah-moon <118171802+minah-moon@users.noreply.github.com>
2023-04-17 09:35:15 +0900

    MoonMinah

--- src/main/java/com/ateam/lionbuy/dto/BuyItemDTO.java
+++ src/main/java/com/ateam/lionbuy/dto/BuyItemDTO.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class BuyItemDTO {
   private String user_email;
   private String pd_name;
-  private Long pay;
+  private Long price;
   private LocalDateTime buy_date;
   private String card_brand;
   private String card_style;
--- src/main/java/com/ateam/lionbuy/service/BuyItemService.java
+++ src/main/java/com/ateam/lionbuy/service/BuyItemService.java
@@ -26,7 +26,7 @@ public interface BuyItemService {
     Buy_item buy_item = Buy_item.builder()
         .user_info(user_info)
         .product(product)
-        .pay(String.valueOf(buyItemDTO.getPay()))
+        .pay(String.valueOf(buyItemDTO.getPrice()))
         .card_brand(buyItemDTO.getCard_brand())
         .card_style(buyItemDTO.getCard_style())
         .build();
@@ -36,7 +36,7 @@ public interface BuyItemService {
   default BuyItemDTO buyItem_build_dto(Buy_item buy_item) {
     BuyItemDTO buyItemDTO = BuyItemDTO.builder()
         .pd_name(buy_item.getProduct().getPd_name())
-        .pay(Long.valueOf(buy_item.getPay()))
+        .price(Long.valueOf(buy_item.getPay()))
         .buy_date(buy_item.getBuy_date())
         .card_brand(buy_item.getCard_brand())
         .card_style(buy_item.getCard_style())
