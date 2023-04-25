15b9702fca974def360d37a6f82a94ffcb52725b
minah-moon <118171802+minah-moon@users.noreply.github.com>
2023-04-25 10:05:18 +0900

    MoonMinah

--- src/main/java/com/ateam/lionbuy/entity/BuyItem.java
+++ src/main/java/com/ateam/lionbuy/entity/BuyItem.java
@@ -2,7 +2,14 @@ package com.ateam.lionbuy.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,26 +22,27 @@ import lombok.ToString;
 @AllArgsConstructor
 @Getter
 @ToString
-public class Wish_item {
+public class Buy_item {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long wno;
+    private Long bno;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_num", referencedColumnName = "user_num")
-    private User_info user_info;
+    private UserInfo user_info;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="pd_name", referencedColumnName = "pd_name")
     private Product product;
 
-    private Long priority;
-    private LocalDateTime choice_date;
+    private Long price;
+    private LocalDateTime buy_date;
+    private String card_brand;
+    private String card_style;
 
     @PrePersist
     public void PrePersist() {
-        choice_date = LocalDateTime.now();
+        buy_date = LocalDateTime.now();
     }
-
 }
